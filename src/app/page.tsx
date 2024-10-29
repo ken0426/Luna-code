6a3fdd8482cb9f6048271923ea216718a877bff8
ken0426 <kenkaitixyou0426@gmail.com>
2024-10-29 22:07:29 +0900

    feat: デフォルトの雛形を作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
+import { CSSProperties } from 'react';
+
+import { COLORS } from '@/styles';
+
+import styles from '../styles/login/login.module.css';
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <div
+      className={styles.contents}
+      style={{ '--background-color': COLORS.GRAY } as CSSProperties}
+    ></div>
+  );
 }
---
+++ src/styles/index.ts
@@ -0,0 +1,5 @@
+export enum COLORS {
+  BLACK = '#111111',
+  WHITE = '#FFFFFF',
+  GRAY = '#262626',
+}
