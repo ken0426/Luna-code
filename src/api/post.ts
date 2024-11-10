827a15532117659fc942cfa517e930e58449b885
ken0426 <kenkaitixyou0426@gmail.com>
2024-11-10 19:30:34 +0900

    refactor: 不要な記述を削除

--- src/api/post.ts
+++ src/api/post.ts
@@ -3,7 +3,6 @@ import { PostSchemaType } from '@/schema/post';
 import { addDoc, collection, deleteDoc, doc } from 'firebase/firestore';
 
 export const createPostApi = async (data: PostSchemaType) => {
-  console.log(data);
   const postsRef = collection(db, 'posts');
   await addDoc(postsRef, data);
 };
