8225e97a1a7aed792d714eab76f76775ca0a6396
ken0426 <kenkaitixyou0426@gmail.com>
2024-11-10 15:25:24 +0900

    feat: サイドバーの実装

--- src/components/layout/MainCustomArea.tsx
+++ src/components/layout/MainCustomArea.tsx
@@ -1,13 +1,39 @@
 import { FC } from 'react';
 
+import { useRouter } from 'next/navigation';
+
+import { COLORS } from '@/styles';
 import { RNChildren } from '@/types';
 
 import styles from '../../styles/area/mainCustomArea.module.css';
 
 const MainCustomArea: FC<RNChildren> = ({ children }) => {
+  const router = useRouter();
   return (
     <div className={styles.area}>
-      <header className={styles.header}></header>
+      <header className={styles.header}>
+        <nav className={styles.nav}>
+          <ul>
+            <li
+              onClick={() => router.push('/home')}
+              style={{ color: COLORS.WHITE }}
+              className={styles.navItem}
+            >
+              ホーム
+            </li>
+            <li style={{ color: COLORS.WHITE }} className={styles.navItem}>
+              投稿する
+            </li>
+            <li
+              onClick={() => router.push('/settings')}
+              style={{ color: COLORS.WHITE }}
+              className={styles.navItem}
+            >
+              設定
+            </li>
+          </ul>
+        </nav>
+      </header>
       <main className={styles.main}>{children}</main>
       <footer className={styles.footer}></footer>
     </div>
